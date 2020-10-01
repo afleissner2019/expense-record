ff7956549031950635d51657d0fbba870d2eedf4
Af <af_ajay@yahoo.com>
2020-10-01 20:07:31 +0200

    Updates

---
+++ precache-manifest.caf0d1b66ce1e283425266f944e9fe6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8100216ca4479200db244dd559f3749",
+    "url": "/expense-record/index.html"
+  },
+  {
+    "revision": "533e81dc1da986baad12",
+    "url": "/expense-record/static/css/main.5c6e62e0.chunk.css"
+  },
+  {
+    "revision": "cec04b6365dd9263438c",
+    "url": "/expense-record/static/js/2.db8ca477.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-record/static/js/2.db8ca477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533e81dc1da986baad12",
+    "url": "/expense-record/static/js/main.3731e8d4.chunk.js"
+  },
+  {
+    "revision": "d478384d1851b53bf16f",
+    "url": "/expense-record/static/js/runtime-main.ba221e8b.js"
+  }
+]);
